5680aa971dd88e8c4608e4ea56528fa9b8c867e8
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-01-29 11:55:06 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+require 'pry'
+
+def prime?(num)
+  i = 2 
+  if num > 1 
+    range = (i..num-1).to_a
+    range.none? do |prime_check|
+      num % prime_check ==0 
+end
+else
+  false 
+end
+end
